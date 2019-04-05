8842e21653cfe13764fdd3de025342ba0d43f5ef
thaisluca <thaisluca@uol.com.br>
2019-04-04 23:11:34 -0300

    Removendo logs de controle

--- permutacao.c
+++ permutacao.c
@@ -7,23 +7,22 @@ Recebe um número a e um b e respode se a é permutação de b.
 
 int main (void){
 	char a[100], b[100];
-	int i, seq = 0,
+	int i, seq = 0, n,
 	contadorA[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
 	contadorB[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 	scanf("%s", a);
 	scanf("%s", b);
 
-	i = 0;
-	while(a[i] != '\0'){
-		contadorA[(int) a[i]] += 1;
-		printf("%d %d\n", a[i], contadorA[a[i]]);
-		i++;
+	n = strlen(a);
+	for(i = 0; i < n; i++){
+		contadorA[a[i]] += 1;
+		printf("%s %d\n", a[i], contadorA[a[i]]);
 	}
 
-	while(b[i] != '\0'){
-		contadorB[(int) a[i]] += 1;
-		i++;
+	n = strlen(b);
+	for(i = 0; i < n; i++){
+		contadorB[b[i]] += 1;
 	}
 
 	for(i = 0; i < 10; i++){
@@ -31,7 +30,6 @@ int main (void){
 		if(contadorA[i] == contadorB[i]) seq += 1;
 	}
 
-	printf("Oi\n");
 	printf("%d\n", seq);
 	if(seq == strlen(a)){
 		printf("É permutação.\n");
