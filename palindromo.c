5cd475b08a8484f61859b4bc2fc6961f100475d0
thaisluca <thaisluca@uol.com.br>
2019-04-04 22:59:06 -0300

    Adicionando flag para controlar prints na ordem correta.

--- palindromo.c
+++ palindromo.c
@@ -7,10 +7,11 @@ Imprime VERDADEIRO se é palíndromo e FALSO caso contrário.
 #include <string.h>
 
 int main(void) {
-	char numero[100] = {'0', '0', '1', '0', '1', '\0'};
-	int i, n;
+	char numero[100];
+	int i, n, ehPalindromo = 1;
 	
 	while(1){
+		ehPalindromo = 1;
 		scanf("%s", numero);
 		if(n < 0) break;
 		
@@ -18,11 +19,14 @@ int main(void) {
 	
 		for(i = 0; i < n; i++){
 			if(numero[i] != numero[n-i-1]){
-				printf("FALSO");
-				return 0;
+				printf("FALSO\n");
+				ehPalindromo = 0;
+				break;
 			}
 		}
-		printf("VERDADEIRO");
+		if(ehPalindromo){
+			printf("VERDADEIRO\n");
+		}
 	}
 	return 0;
 }
