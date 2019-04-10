d6c131f2cf30763e6cb386bf2fc04ee3f5976090
thais.almeida <thais.almeida@radixeng.com.br>
2019-04-10 12:01:55 -0300

    Ajustando problema dos prefixos.

--- prefixo.c
+++ prefixo.c
@@ -9,26 +9,30 @@ Programa que, dada uma cadeia de caracteres e um inteiro n, retira o
 void retira_inicio_n(char *str, int n);
 
 int main(void){
-	char *str;
+	//char *str;
 	int n;
 
 	scanf("%d", &n);
 	str = (char*) malloc(sizeof(char)*n);
 	if(!str) return 1;
+
 	scanf("\n \n%s", str);
 	if(n > strlen(str)){
 		printf("Erro\n");
 		return 0;
 	}
 	retira_inicio_n(str, n);
-	puts(str);
+	printf("%s\n", str);
+	return 0;
 }
 
 void retira_inicio_n(char *str, int n){
 	char *novaString;
 	int i;
-	for(i = n; i < strlen(str); i++){
-		novaString[i] = str[i];
+	novaString = (char *) malloc(sizeof(char)*(strlen(str)-n));
+	if(!novaString) return 1;
+	for(i = 0; i < strlen(str); i++){
+		novaString[i] = str[n + i];
 	}
 	str = strcpy(str, novaString);
 }
