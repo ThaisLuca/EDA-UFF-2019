b03d08c9a7bf2f76cff406f71e367c65fcd5f4aa
thaisluca <thaisluca@uol.com.br>
2019-04-14 19:42:34 -0300

    Atualizando sudoku e adicionando leitura de arquivo.

--- Lista4/sudoku.c
+++ Lista4/sudoku.c
@@ -1,50 +1,93 @@
 /*
+A partir de uma matriz quadrada de dimensão 9, verifica se a matriz é uma solução
+válida para o Sudoku. Cada linha, cada coluna e cada contém os números de 1 a 9
+somente uma vez.
 */
 
 #include <stdio.h>
 
-#define DIM 3
+#define DIM 9
 
-void lerMatriz(int matriz[DIM][DIM]);
+void lerMatriz(int matriz[DIM][DIM], FILE *arquivo);
+void zerarContador(int contador[10]);
 
 int main (void){
 
-	int matriz[DIM][DIM], solucao = 1, i, j;
-	int contador[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int matriz[DIM][DIM], i, j, elemento;
+	int contador[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	FILE *arquivo;
 
-	lerMatriz(matriz);
+	arquivo = fopen("entradaSudoku.txt", "r");
+	if(arquivo == NULL){
+		printf("Erro ao ler arquivo de entrada.\n");
+		return 1;
+	}
 
-	for(i = 0; i > DIM; i++){
+	lerMatriz(matriz, arquivo);
+	fclose(arquivo);
+
+	//Verifica as linhas
+	for(i = 0; i < DIM; i++){
+		zerarContador(contador);
 		for(j = 0; j < DIM; j++){
-			if(contador[j] > 0){
-				solucao = 0;
-				break;
+			elemento = matriz[i][j];
+			if(contador[elemento] > 0){
+				printf("Não é uma solução válida: elemento repetido em uma das linhas.\n");
+				return 0;
 			} else {
-				contador[j] += 1;
+				contador[elemento] += 1;
 			}
 		}
-		if(contador[i] > 0){
-			solucao = 0;
-			break;
-		} else {
-			contador[i] += 1;
+	}
+
+	//Verifica as colunas
+	for(j = 0; j < DIM; j++){
+		zerarContador(contador);
+		for(i = 0; i < DIM; i++){
+			elemento = matriz[i][j];
+			if(contador[elemento] > 0){
+				printf("Não é uma solução válida: elemento repetido em uma das colunas.\n");
+				return 0;
+			} else {
+				contador[elemento] += 1;
+			}
 		}
 	}
 
-	if(solucao){
-		printf("É solução!\n");
-	} else {
-		printf("Não é solução!\n");
+	//Verifica os blocos
+	int bloco = 0;
+	int limiteBloco = DIM/3;
+	for(bloco = 0; bloco < DIM; bloco++){
+		zerarContador(contador);
+		for(i = bloco; i < i + limiteBloco; i++){
+			for(j = bloco; j < j + limiteBloco; j++){
+				elemento = matriz[i][j];
+				if(contador[elemento] > 0){
+					printf("Não é uma solução válida: elemento repetido em um dos blocos da matriz.\n");
+					return 0;
+				} else {
+					contador[elemento] += 1;
+				}
+			}
+		}
 	}
+
 
 	return 0;
 }
 
-void lerMatriz(int matriz[DIM][DIM]){
+void lerMatriz(int matriz[DIM][DIM], FILE *arquivo){
 	int i, j;
 	for(i = 0; i < DIM; i++){
 		for(j = 0; j < DIM; j++){
-			scanf("%d", &matriz[i][j]);
+			fscanf(arquivo, "%d", &matriz[i][j]);
 		}
+	}
+}
+
+void zerarContador(int contador[10]){
+	int i;
+	for(i = 0; i < 10; i++){
+		contador[i] = 0;
 	}
 }
